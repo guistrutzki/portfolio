80a39713904aa90c225d3f52203c6c0844d1ee95
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2019-07-08 02:47:44 -0300

    Feat: background video and typed.js added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
+/* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
+import { Reset } from 'styled-reset';
+import Typed from 'react-typed';
+import { VideoContainer, Container } from './styles';
+// import bgImage from './assets/progamming.jpg';
 
-import { Title } from './styles';
+const heart = '&hearts';
 
-function App() {
-  return (
-    <div>
-      <Title>Hello</Title>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <Reset />
+        <div>
+          <VideoContainer>
+            <div />
+            <video autoPlay loop>
+              <source
+                src="https://dyupuz6787mty.cloudfront.net/assets/vid/Hello-World.mp4"
+                type="video/mp4"
+              />
+            </video>
+          </VideoContainer>
+
+          <Container>
+            <h4>Hi, I'm Guilherme Strutzki</h4>
+            <h4>
+              <Typed
+                strings={[
+                  "I'm front end developer.",
+                  'I transform images and templates in websites.',
+                  'I create apps Android and iOS.',
+                  'And I love do it ❤',
+                ]}
+                typeSpeed={60}
+                backSpeed={50}
+                loop
+                backDelay={1000}
+              />
+            </h4>
+          </Container>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,80 @@
 import styled from 'styled-components';
+import { device } from './device';
+import bgImg from './assets/progamming.jpg';
 
-export const Title = styled.h1``;
+export const VideoContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background: #000;
+  z-index: 0;
+
+  div {
+    z-index: 100;
+    position: absolute;
+    background: rgba(45, 56, 58, 0.5);
+    width: 100%;
+    height: 100%;
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+  }
+`;
+
+export const Container = styled.div`
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+
+  h4 {
+    margin-bottom: 20px;
+    font-size: 2rem;
+    font-weight: 700;
+    font-family: Lato, Arial, sans-serif;
+    color: #fff;
+    font-size: 50;
+    position: relative;
+    top: 300px;
+    left: 10px;
+    text-align: center;
+    justify-content: center;
+  }
+`;
+
+export const Header = styled.section`
+  background: url(${bgImg}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  min-height: 800px;
+  display: flex;
+  flex-direction: column;
+  opacity: 0.9;
+  align-content: center;
+  justify-content: center;
+
+  @media ${device.mobileM} {
+    min-height: 500px;
+  }
+
+  h4 {
+    margin-bottom: 20px;
+    color: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 20px;
+    text-align: center;
+
+    @media ${device.desktopL} {
+      font-size: 50px;
+    }
+  }
+`;
